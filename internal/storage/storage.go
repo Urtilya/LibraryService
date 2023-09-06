55e1bf781e23879ff3c825b1035692bd63f90013
Ilya <johndoe@example.com>
2023-09-06 18:39:06 +0300

    db

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -17,10 +17,12 @@ func NewStorage(db *sqlx.DB) *Storage {
 }
 
 func (s *Storage) GetBooks() (models.Book, error) {
+	//select  `Authors`.name from `Books` join `Relevant` on `Relevant`.id_book =`Books`.id join `Authors` on `Authors`.id = `Relevant`.id_author WHERE `Books`.name = 'День черных звезд'
 	_, err := s.db.Query("SELECT * FROM books")
 	return models.Book{}, err
 }
 func (s *Storage) GetAuthors() (models.Author, error) {
+	//select  `Books`.name from `Authors` join `Relevant` on `Relevant`.id_author = `Authors`.id join `Books` on `Books`.id = `Relevant`.id_book WHERE `Authors`.name = 'Иар Эльтеррус'
 	_, err := s.db.Query("SELECT * FROM authors")
 	return models.Author{}, err
 }
